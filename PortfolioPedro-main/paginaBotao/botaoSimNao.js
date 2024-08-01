0aea8afba82aeb7cc44d9dbceb577b4abd2f0712
Pedro12421 <150083495+Pedro12421@users.noreply.github.com>
2024-08-01 18:43:27 -0300

    Add files via upload

---
+++ PortfolioPedro-main/paginaBotao/botaoSimNao.js
@@ -0,0 +1,11 @@
+
+// //é um conjunto de passos que executa uma tarefa de forma ordenada,e também tem um objetivo ao final
+// function botaoSim() {
+//     window.location.href = "http://www.w3schools.com";
+// }
+
+var botaoNao = document.querySelector("#botaoNao");
+botaoNao.addEventListener("click",function(){
+    alert("to funcionando")
+})
+alert("oi")
